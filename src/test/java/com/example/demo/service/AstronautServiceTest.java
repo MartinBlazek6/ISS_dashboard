894a4de7c8a43e137f6051ac2fe45c9a1944af5e
Martin Blazek <martinblazek6@gmail.com>
2023-10-12 16:11:04 +0200

    tests

---
+++ src/test/java/com/example/demo/service/AstronautServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AstronautServiceTest {
+
+    @Test
+    void getAstronautByNameAndLastName() {
+        assertEquals(1,1);
+    }
+}
